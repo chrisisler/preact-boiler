252abc7585b7b02f6e916642144bbbadd1d6fdbd
chrisisler <christopherisler1@gmail.com>
2017-06-22 18:08:05 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { h, Component } from 'preact';
+import './App.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div id='app'>
+            </div>
+        );
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { h, render } from 'preact';
+import './normalize.css';
+import App from './App';
+
+render(<App />, document.body);
